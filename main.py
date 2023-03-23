0a6b237009b5ba296b5863a6189fdb232e80e66b
AidenVu1 <100857445+AidenVu1@users.noreply.github.com>
2023-03-23 14:16:03 +0000

    hi

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-
+import math
+d = 0
+pi=math.pi
+while True:
+    try:
+      integer=float(input("what is the diameter\n"))/2
+      if integer<0:
+        print('please enter a positive number')
+        continue
+        
+    except ValueError:
+        print("Please enter a number")
+        continue
+      
+    else:
+        pi = math.pi
+        c = 2 * pi * d
+        a = pi * (d ** 2)
+        print('circumference = ',c)
+        print('area = ',a)
+        break
